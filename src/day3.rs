7ff06a0966c09555ad1706c445236a297ab3bf40
Sander in 't Veld <sveld@block-tech.io>
2024-12-04 00:09:41 -0500

    Day 3, both parts together in 18us.

--- src/day3.rs
+++ src/day3.rs
@@ -1,15 +1,9 @@
 use aoc_runner_derive::aoc;
-use regex::Regex;
 
 #[aoc(day3, part1)]
 pub fn part1(input: &str) -> u32
 {
-	// "mul\(([0-9]{1,3}),([0-9]{1,3})\)"
-	part1_(input.as_bytes())
-}
-
-fn part1_(input: &[u8]) -> u32
-{
+	let input = input.as_bytes();
 	let mut sum = 0;
 	let mut i = 0;
 	let n = input.len();
@@ -65,7 +59,8 @@ fn part1_(input: &[u8]) -> u32
 				if is_match
 				{
 					let a = (next[0] - b'0') as u32;
-					let mut b = (next[2] - b'0') as u32 * 10 + (next[3] - b'0') as u32;
+					let mut b =
+						(next[2] - b'0') as u32 * 10 + (next[3] - b'0') as u32;
 					let j = i;
 					while i < n
 					{
@@ -76,7 +71,8 @@ fn part1_(input: &[u8]) -> u32
 						{
 							b'0'..=b'9' =>
 							{
-								if i >= j + 2 {
+								if i >= j + 2
+								{
 									continue 'clean;
 								}
 								b = b * 10 + (x - b'0') as u32;
@@ -86,7 +82,8 @@ fn part1_(input: &[u8]) -> u32
 								sum += a * b;
 								continue 'clean;
 							}
-							_ => {
+							_ =>
+							{
 								i -= 1;
 								continue 'clean;
 							}
@@ -107,7 +104,8 @@ fn part1_(input: &[u8]) -> u32
 					& (next[3] <= b'9');
 				if is_match
 				{
-					let a = (next[0] - b'0') as u32 * 10 + (next[1] - b'0') as u32;
+					let a =
+						(next[0] - b'0') as u32 * 10 + (next[1] - b'0') as u32;
 					let mut b = (next[3] - b'0') as u32;
 					let j = i;
 					while i < n
@@ -119,7 +117,8 @@ fn part1_(input: &[u8]) -> u32
 						{
 							b'0'..=b'9' =>
 							{
-								if i >= j + 3 {
+								if i >= j + 3
+								{
 									continue 'clean;
 								}
 								b = b * 10 + (x - b'0') as u32;
@@ -129,7 +128,8 @@ fn part1_(input: &[u8]) -> u32
 								sum += a * b;
 								continue 'clean;
 							}
-							_ => {
+							_ =>
+							{
 								i -= 1;
 								continue 'clean;
 							}
@@ -164,7 +164,8 @@ fn part1_(input: &[u8]) -> u32
 						{
 							b'0'..=b'9' =>
 							{
-								if i >= j + 4 {
+								if i >= j + 4
+								{
 									continue 'clean;
 								}
 								b = b * 10 + (x - b'0') as u32;
@@ -174,7 +175,8 @@ fn part1_(input: &[u8]) -> u32
 								sum += a * b;
 								continue 'clean;
 							}
-							_ => {
+							_ =>
+							{
 								i -= 1;
 								continue 'clean;
 							}
@@ -187,7 +189,8 @@ fn part1_(input: &[u8]) -> u32
 					continue 'clean;
 				}
 			}
-			_ => {
+			_ =>
+			{
 				i -= 4;
 				continue 'clean;
 			}
@@ -199,27 +202,237 @@ fn part1_(input: &[u8]) -> u32
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> u32
 {
-	let re =
-		Regex::new(r"(do\(\))|(don't\(\))|mul\(([0-9]{1,3}),([0-9]{1,3})\)")
-			.unwrap();
+	let input = input.as_bytes();
 	let mut sum = 0;
-	let mut is_active = true;
-	for c in re.captures_iter(input)
+	let mut i = 0;
+	let n = input.len();
+	const MUL: u32 = u32::from_be_bytes([b'm', b'u', b'l', b'(']);
+	const DON: u32 = u32::from_be_bytes([b'd', b'o', b'n', b'\'']);
+	'clean: while i + 7 < n
 	{
-		if c.get(1).is_some()
+		// Safety: we just checked that input[i + 7] was not out of bounds.
+		let view: &[u8; 4] = unsafe {
+			input.get_unchecked(i..).first_chunk().unwrap_unchecked()
+		};
+		let mut view = u32::from_be_bytes(*view);
+		i += 4;
+		while view != MUL
 		{
-			is_active = true;
+			if view == DON
+			{
+				if i + 2 < n
+				{
+					// We have just checked that input[i + 2] was not out of bounds.
+					let next: &[u8; 3] = unsafe {
+						input
+							.get_unchecked(i..)
+							.first_chunk()
+							.unwrap_unchecked()
+					};
+					if *next == [b't', b'(', b')']
+					{
+						i += 3;
+						while i + 4 + 8 - 1 < n
+						{
+							// We have just checked that input[i + 3] was not out of bounds.
+							let next: &[u8; 4] = unsafe {
+								input
+									.get_unchecked(i..)
+									.first_chunk()
+									.unwrap_unchecked()
+							};
+							if *next == [b'd', b'o', b'(', b')']
+							{
+								i += 4;
+								continue 'clean;
+							}
+							i += 1;
+						}
+						return sum;
+					}
+				}
+				else
+				{
+					return sum;
+				}
+			}
+			if i + 4 < n
+			{
+				// Safety: we just checked that input[i + 4] was not out of bounds.
+				let x = unsafe { *input.get_unchecked(i) };
+				view = (view << 8) | (x as u32);
+				i += 1;
+			}
+			else
+			{
+				return sum;
+			}
 		}
-		else if c.get(2).is_some()
+
+		// We have just seen "mul(" and we have checked that input[i + 3] was not out of bounds.
+		let next: &[u8; 4] = unsafe {
+			input.get_unchecked(i..).first_chunk().unwrap_unchecked()
+		};
+		let next_view = u32::from_be_bytes(*next);
+		i += 4;
+		let _: std::convert::Infallible = match next_view & 0xF0F0F0F0
 		{
-			is_active = false;
-		}
-		else if is_active
-		{
-			let a: u32 = c.get(3).unwrap().as_str().parse().unwrap();
-			let b: u32 = c.get(4).unwrap().as_str().parse().unwrap();
-			sum += a * b;
-		}
+			0x30203020 =>
+			{
+				let is_match: bool = (next[0] <= b'9')
+					& (next[1] == b',')
+					& (next[2] <= b'9')
+					& (next[3] == b')');
+				let a = (next[0] - b'0') as u32;
+				let b = (next[2] - b'0') as u32;
+				sum += u32::from(is_match) * a * b;
+				continue 'clean;
+			}
+			0x30203030 =>
+			{
+				let is_match = (next[0] <= b'9')
+					& (next[1] == b',')
+					& (next[2] <= b'9')
+					& (next[3] <= b'9');
+				if is_match
+				{
+					let a = (next[0] - b'0') as u32;
+					let mut b =
+						(next[2] - b'0') as u32 * 10 + (next[3] - b'0') as u32;
+					let j = i;
+					while i < n
+					{
+						// Safety: we just checked that input[i] is not out of bounds.
+						let x = unsafe { *input.get_unchecked(i) };
+						i += 1;
+						match x
+						{
+							b'0'..=b'9' =>
+							{
+								if i >= j + 2
+								{
+									continue 'clean;
+								}
+								b = b * 10 + (x - b'0') as u32;
+							}
+							b')' =>
+							{
+								sum += a * b;
+								continue 'clean;
+							}
+							_ =>
+							{
+								i -= 1;
+								continue 'clean;
+							}
+						}
+					}
+					return sum;
+				}
+				else
+				{
+					continue 'clean;
+				}
+			}
+			0x30302030 =>
+			{
+				let is_match = (next[0] <= b'9')
+					& (next[1] <= b'9')
+					& (next[2] == b',')
+					& (next[3] <= b'9');
+				if is_match
+				{
+					let a =
+						(next[0] - b'0') as u32 * 10 + (next[1] - b'0') as u32;
+					let mut b = (next[3] - b'0') as u32;
+					let j = i;
+					while i < n
+					{
+						// Safety: we just checked that input[i] is not out of bounds.
+						let x = unsafe { *input.get_unchecked(i) };
+						i += 1;
+						match x
+						{
+							b'0'..=b'9' =>
+							{
+								if i >= j + 3
+								{
+									continue 'clean;
+								}
+								b = b * 10 + (x - b'0') as u32;
+							}
+							b')' =>
+							{
+								sum += a * b;
+								continue 'clean;
+							}
+							_ =>
+							{
+								i -= 1;
+								continue 'clean;
+							}
+						}
+					}
+					return sum;
+				}
+				else
+				{
+					continue 'clean;
+				}
+			}
+			0x30303020 =>
+			{
+				let is_match = (next[0] <= b'9')
+					& (next[1] <= b'9')
+					& (next[2] <= b'9')
+					& (next[3] == b',');
+				if is_match
+				{
+					let a = (next[0] - b'0') as u32 * 100
+						+ (next[1] - b'0') as u32 * 10
+						+ (next[2] - b'0') as u32;
+					let mut b = 0;
+					let j = i;
+					while i < n
+					{
+						// Safety: we just checked that input[i] is not out of bounds.
+						let x = unsafe { *input.get_unchecked(i) };
+						i += 1;
+						match x
+						{
+							b'0'..=b'9' =>
+							{
+								if i >= j + 4
+								{
+									continue 'clean;
+								}
+								b = b * 10 + (x - b'0') as u32;
+							}
+							b')' =>
+							{
+								sum += a * b;
+								continue 'clean;
+							}
+							_ =>
+							{
+								i -= 1;
+								continue 'clean;
+							}
+						}
+					}
+					return sum;
+				}
+				else
+				{
+					continue 'clean;
+				}
+			}
+			_ =>
+			{
+				i -= 4;
+				continue 'clean;
+			}
+		};
 	}
 	sum
 }
